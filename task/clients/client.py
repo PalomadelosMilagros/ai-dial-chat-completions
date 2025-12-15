c6439e94aa90a284bc2ad7b39bb6b4d13596f12c
Paloma <paloma_delosmilagros@epamneoris.com>
2025-12-15 12:22:01 +0100

    codigo completo

--- task/clients/client.py
+++ task/clients/client.py
@@ -13,7 +13,16 @@ class DialClient(BaseClient):
         #TODO:
         # Documentation: https://pypi.org/project/aidial-client/ (here you can find how to create and use these clients)
         # 1. Create Dial client
+        self._client = Dial(
+            base_url=DIAL_ENDPOINT,
+            api_key=self._api_key,
+        )
         # 2. Create AsyncDial client
+        self._async_client = AsyncDial(
+            base_url=DIAL_ENDPOINT,
+            api_key=self._api_key,
+        )
+        
 
     def get_completion(self, messages: list[Message]) -> Message:
         #TODO:
@@ -21,7 +30,18 @@ class DialClient(BaseClient):
         #    Hint: to unpack messages you can use the `to_dict()` method from Message object
         # 2. Get content from response, print it and return message with assistant role and content
         # 3. If choices are not present then raise Exception("No choices in response found")
-        raise NotImplementedError
+        response = self._client.chat.completions.create(
+            deployment_name=self._deployment_name,
+            stream=False,
+            messages=[msg.to_dict() for msg in messages],
+        )
+
+        if choices := response.choices:
+            if message := choices[0].message:
+                print(message.content)
+                return Message(Role.AI, message.content)
+
+        raise Exception("No choices in response found")
 
     async def stream_completion(self, messages: list[Message]) -> Message:
         #TODO:
@@ -32,4 +52,19 @@ class DialClient(BaseClient):
         # 4. Print content chunk and collect it contents array
         # 5. Print empty row `print()` (it will represent the end of streaming and in console we will print input from a new line)
         # 6. Return Message with assistant role and message collected content
-        raise NotImplementedError
+          chunks = await self._async_client.chat.completions.create(
+            deployment_name=self._deployment_name,
+            messages=[msg.to_dict() for msg in messages],
+            stream=True,
+        )
+
+          contents = []
+          async for chunk in chunks:
+                if chunk.choices and len(chunk.choices) > 0:
+                    delta = chunk.choices[0].delta
+                    if delta and delta.content:
+                        print(delta.content, end='')
+                        contents.append(delta.content)
+
+          print()
+          return Message(Role.AI, ''.join(contents))
